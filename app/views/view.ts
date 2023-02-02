ad7836ed8c53ef5fdca49f0324bf35dc79237b99
vitorr7df <vitor.rochadf@gmail.com>
2023-02-02 17:44:21 -0300

    class abstract extend

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,4 +1,4 @@
-export class View<T> {
+export abstract class View<T> {
     protected elemento: HTMLElement;
     constructor(seletor: string) {
         this.elemento = document.querySelector(seletor);
@@ -9,8 +9,5 @@ export class View<T> {
         this.elemento.innerHTML = template;
     }
 
-    template(model: T): string {
-       throw Error('Classe filha precisa implementar o método')
-    }
-
+    abstract template(model: T): string;
 }
