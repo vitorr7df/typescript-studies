5b30ea5a9f69e56fcb973968d00308498f064c82
vitorr7df <vitor.rochadf@gmail.com>
2023-02-02 17:07:02 -0300

    Refatorando

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -1,11 +1,8 @@
 import { Negociacoes } from "../models/negociacoes.js";
+import { View } from "./view.js";
 
-export class NegociacoesView {
+export class NegociacoesView extends View<Negociacoes>{
 
-    private elemento: HTMLElement;
-    constructor(seletor: string) {
-        this.elemento = document.querySelector(seletor);
-    }
     template(model: Negociacoes): string {
         return `
         <table class="table table-hover table-bordered">
@@ -29,11 +26,5 @@ export class NegociacoesView {
             </tbody>
         </table> 
         `;
-    }
-
-    update(model: Negociacoes): void {
-        const template = this.template(model);
-        console.log(template);
-        this.elemento.innerHTML = template;
     }
 }
---
+++ app/views/view.ts
@@ -0,0 +1,16 @@
+export class View<T> {
+    protected elemento: HTMLElement;
+    constructor(seletor: string) {
+        this.elemento = document.querySelector(seletor);
+    }
+
+    update(model: T): void {
+        const template = this.template(model);
+        this.elemento.innerHTML = template;
+    }
+
+    template(model: T): string {
+       throw Error('Classe filha precisa implementar o método')
+    }
+
+}
