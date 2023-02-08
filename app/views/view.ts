42d9bb7d0bb3467a038b45159ac1d11d3331c5ef
vitorr7df <vitor.rochadf@gmail.com>
2023-02-08 10:56:27 -0300

    removeComments

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,11 +1,20 @@
 export abstract class View<T> {
     protected elemento: HTMLElement;
-    constructor(seletor: string) {
+    private escapar = false;
+
+    constructor(seletor: string, escapar?: boolean) {
         this.elemento = document.querySelector(seletor);
+        if (escapar) {
+            this.escapar = escapar;
+        }
     }
 
     public update(model: T): void {
-        const template = this.template(model);
+        let template = this.template(model);
+        if (this.escapar) { 
+            template = template
+                .replace(/<script>[\s\S]*?<\/script>/, ''); //remover adição de script no console
+        }
         this.elemento.innerHTML = template;
     }
 
--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -6,7 +6,7 @@ import { DiasDaSemana } from '../enums/dias-da-semana.js';
 export class NegociacaoController {
     constructor() {
         this.negociacoes = new Negociacoes();
-        this.negociacoesView = new NegociacoesView('#negociacoesView');
+        this.negociacoesView = new NegociacoesView('#negociacoesView', true);
         this.mensagemView = new MensagemView('#mensagemView');
         this.SABADO = 6;
         this.DOMINGO = 0;
