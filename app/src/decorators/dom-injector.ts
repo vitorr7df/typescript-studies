fd1149c7632041d8c937b6cd13bd50f97057da81
vitorr7df <vitor.rochadf@gmail.com>
2023-02-10 10:04:52 -0300

    ajust dom-injector

--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -1,11 +1,17 @@
 export function domInjector(seletor: string) {
+    
     return function(target: any, propertyKey: string) {
         console.log(`Modificando protype ${target.constructor.name} 
             e adicionando getter para a propriedade ${propertyKey}`);
+            
+        let elemento: HTMLElement;
+
         const getter = function() {
-            const elemento = document.querySelector(seletor);
-            console.log(`Buscando elemento do DOM com o seletor 
-                ${seletor} para injetar em ${propertyKey}`);
+            if (!elemento) {
+                elemento = <HTMLElement>document.querySelector(seletor);
+                console.log(`Buscando elemento do DOM com o seletor 
+                    ${seletor} para injetar em ${propertyKey}`);
+            }       
             return elemento;
         }
         Object.defineProperty(target, propertyKey, {
