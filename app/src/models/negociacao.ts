019080dc3983aa52fdbb06faca763255b29576f4
vitorr7df <vitor.rochadf@gmail.com>
2023-02-13 16:44:24 -0300

    comparando negociacoes

--- app/src/models/negociacao.ts
+++ app/src/models/negociacao.ts
@@ -1,13 +1,11 @@
 import { Imprimivel } from "../utils/imprimivel.js";
 
-export class Negociacao extends Imprimivel {
+export class Negociacao implements Imprimivel {
     constructor(
         private _data: Date, 
         public readonly quantidade: number, 
         public readonly valor: number
-    ) {
-        super();
-    }
+    ) {}
 
     public static criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
         const exp = /-/g;
@@ -33,4 +31,11 @@ export class Negociacao extends Imprimivel {
             Valor: ${this.valor}
         `;
     }
+
+    public ehIgual(negociacao: Negociacao): boolean {
+        return this.data.getDate() === negociacao.data.getDate() 
+            && this.data.getMonth() === negociacao.data.getMonth() 
+            && this.data.getFullYear() === negociacao.data.getFullYear();
+    }
+
 }
